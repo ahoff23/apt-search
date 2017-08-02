4672013fba8b985b3ccf97d6f0f47448c3b8fc5b
Andrew Hoff <andrew.h.hoff@gmail.com>
2017-08-01 20:23:12 -0700

    Added travel time filter class.

---
+++ src/MapFilter.java
@@ -0,0 +1,147 @@
+import com.google.gson.Gson;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.HashSet;
+
+public class MapFilter
+{
+    private class JsonData
+    {
+        private class Distance
+        {
+            int value;
+            String text;
+        }
+
+        private class Duration
+        {
+            int value;
+            String text;
+        }
+
+        private class Element
+        {
+            String status;
+            Distance duration;
+            Duration distance;
+        }
+
+        private class Row
+        {
+            ArrayList<Element> elements;
+        }
+
+        String status;
+        ArrayList<String> origin_addresses;
+        ArrayList<String> destination_addresses;
+        ArrayList<Row> rows;
+    }
+
+    /**
+     * Constructor.
+     */
+    public MapFilter()
+    {
+        addresses = new HashSet<>();
+    }
+
+    /**
+     * Add a set of addresses to the member set of addresses.
+     *
+     * @param str An address to add.
+     */
+    public void add(final String str)
+    {
+        addresses.add(str);
+    }
+
+    /**
+     * Filter all addresses against a target address and travel time.
+     *
+     * @param destination The destination address.
+     * @param max_travel_time The max travel time to filter against. Units in
+     *        minutes.
+     */
+    public void filterAddresses(final String destination,
+                                final int max_travel_time) throws IOException
+    {
+        /*
+         * Store a user agent for the HTTP request.
+         */
+        final String user_agent =
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 " +
+            "(KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36";
+
+        /*
+         * Key to use for the google maps API.
+         */
+        final String key = "";
+
+        /*
+         * Iterate over each address.
+         */
+        for(Iterator<String> itr = addresses.iterator(); itr.hasNext();)
+        {
+            String address = itr.next();
+
+            /*
+             * Format the addresses before searching.
+             */
+            final String formatted_address = address.replaceAll(" ", "+");
+            final String formatted_destination =
+                destination.replaceAll(" ", "+");
+
+            final String url =
+                "https://maps.googleapis.com/maps/api/distancematrix/json?" +
+                "origins=" + formatted_address +
+                "&destinations=" + formatted_destination +
+                "&key=" + key;
+
+            /*
+             * Get the travel time between the address and
+             * the target. If the travel time exceeds the maximum travel time,
+             * remove the address.
+             */
+            InputStream stream = new URL(url).openStream();
+            Reader reader = new InputStreamReader(stream, "UTF-8");
+            JsonData data = new Gson().fromJson(reader, JsonData.class);
+            if (data.rows.get(0).elements.get(0).duration.value / 60 >
+                max_travel_time)
+            {
+                itr.remove();
+            }
+        }
+    }
+
+    /**
+     * Get all addresses.
+     *
+     * @return The current set of addresses.
+     */
+    public HashSet<String> getAddresses()
+    {
+        return new HashSet<String>(addresses);
+    }
+
+    public static void main(String[] args) throws IOException
+    {
+        MapFilter filter = new MapFilter();
+        filter.add("328 E 11th Street New York NY");
+        filter.add("103 Avenue A New York NY");
+        filter.add("5023 22nd Seattle WA");
+
+        filter.filterAddresses("100 6th Ave New York NY", 20);
+
+        for (String str : filter.addresses)
+        {
+            System.out.println(str);
+        }
+    }
+
+    private HashSet<String> addresses;
+}
