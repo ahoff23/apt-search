8c2f9a1d8d0e8cda99189c338b7063ad43784072
Andrew Hoff <andrew.h.hoff@gmail.com>
2017-08-06 11:31:09 -0700

    Update Main.java to reflect changes in AddressParser and MapFilter.

--- src/Main.java
+++ src/Main.java
@@ -12,26 +12,18 @@ public class Main
         /*
          * Get all addresses in Manhattan.
          */
-        HashSet<String> addrs =
+        HashSet<AddressParser.UrlAddress> addrs =
                 AddressParser.getAddresses("manhattan", 3500, 4000, 2);
-
-        /*
-         * Pass all addresses into the filter.
-         */
-        MapFilter filter = new MapFilter();
-        for (String addr : addrs)
-            filter.add(addr);
 
         /*
          * Filter against the distance to a desired location.
          */
-        filter.filterAddresses("100 6th Ave New York NY", 20);
+        MapFilter.filterAddresses(addrs, "100 6th Ave New York NY", 20);
 
         /*
          * Print the list of addresses.
          */
-        HashSet<String> addresses = filter.getAddresses();
-        for (String addr : addresses)
-            System.out.println(addr);
+        for (AddressParser.UrlAddress addr : addrs)
+            System.out.println(addr.address);
     }
 }
